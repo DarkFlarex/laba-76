b23fa426d345919ae4486e9b7c63937451ad6d22
DarkFlarex <darkflarexxx@gmail.com>
2024-08-12 04:06:29 +0600

    В роуте теперь прописываем get запрос для получение сообщений и ее фильтрация по дате и времени, возвращающая сообщения, созданные после указанной даты.

--- chat-api/routers/messages.ts
+++ chat-api/routers/messages.ts
@@ -1,8 +1,23 @@
-import express, {query} from "express";
+import express from "express";
 import fileDb from "../fileDb";
 import {MessageMutation} from "../types";
 
 const messagesRouter = express.Router();
+
+messagesRouter.get('/', async (req,res)=>{
+    const messages = await fileDb.getItems();
+    const queryDate = req.query.datetime as string;
+
+    if (queryDate) {
+        const date = new Date(queryDate);
+        if (isNaN(date.getTime())) {
+            return res.status(400).send({ error: 'The date is incorrect' });
+        }
+        const newMessages = messages.filter(message => new Date(message.datetime) > date);
+        return res.send(newMessages);
+    }
+    res.send(messages.slice(-30));
+});
 
 messagesRouter.post('/', async (req, res)=>{
     if(!req.body.author || !req.body.message){
